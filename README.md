cb7f6859daa9ff86f28b98ef08bd0807a09fd4be
BLasan <benuraab@gmail.com>
2020-04-29 11:42:20 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## About App
+
+- This is a Admin Dashboard for pharmacies to handle prescriptions online.
+
+## Prerequisites
+
+- Install node 12.x
+
+## Installation
+
+- git clone https://github.com/BLasan/AL-Web-App.git
+
+- npm install -g @angular/cli@latest
+
+- npm install
+
+- npm start
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { environment } from '../../src/environments/environment.prod';
+import { AppComponent } from './app.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import {
+  AgmCoreModule
+} from '@agm/core';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleMaps
+    })
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
