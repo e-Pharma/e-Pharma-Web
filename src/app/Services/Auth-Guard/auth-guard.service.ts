9845f7f208c92f816196e814eef90f5a38c7cc65
BLasan <benuraab@gmail.com>
2020-06-25 17:16:03 +0530

    feat: add auth-guard for routings

---
+++ src/app/Services/Auth-Guard/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService  implements CanActivate {
+
+  constructor(private router: Router){}
+  canActivate(){
+    console.log(localStorage.getItem('expiresAt'))
+    if(localStorage.getItem('token') !== null && (parseInt(localStorage.getItem('expiresAt')) >= new Date().getTime())) {
+      return true
+    } else {
+      this.router.navigate(['../login']);
+    }
+  }
+
+}
