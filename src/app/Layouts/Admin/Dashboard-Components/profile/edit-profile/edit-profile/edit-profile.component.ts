c9d5e22286f4cb2b4777424d27a980c3cddc3701
ThiliniUdari <thilinihk96@gmail.com>
2020-06-29 19:59:31 +0530

    customer-edit-profile added

---
+++ src/app/Layouts/Admin/Dashboard-Components/profile/edit-profile/edit-profile/edit-profile.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css']
+})
+export class EditProfileComponent implements OnInit {
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
